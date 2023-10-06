f3bb39869175e9930c7c3b015f3fc0685ef05bc1
Patrick Farias <patrickfarias@live.com>
2023-10-06 16:19:04 -0300

    Alteração do número limite de 10 para 100

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ let paragrafo = document.querySelector('p'); //
 paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
 Essa forma anterior não é errado, mas existem boas práticas para reduzir as linhas do código e facilitar a execução do código.*/
 let listaNumerosSorteados = [];
-let numeroLimite = 10;
+let numeroLimite = 100;
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
 
@@ -17,7 +17,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
 
 exibirMensagemInicial();
