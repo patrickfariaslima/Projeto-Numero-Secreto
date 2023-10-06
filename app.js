b0fb2808f4ee984080afc7d9d899c8233bfc60f7
Patrick Farias <patrickfarias@live.com>
2023-10-06 16:16:45 -0300

    Projeto inicial - Jogo de Adivinhar o número secreto

---
+++ app.js
@@ -0,0 +1,74 @@
+/*let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Jogo do número secreto';
+
+let paragrafo = document.querySelector('p'); // 
+paragrafo.innerHTML = 'Escolha um número entre 1 e 10';
+Essa forma anterior não é errado, mas existem boas práticas para reduzir as linhas do código e facilitar a execução do código.*/
+let listaNumerosSorteados = [];
+let numeroLimite = 10;
+let numeroSecreto = gerarNumeroAleatorio();
+let tentativas = 1;
+
+function exibirTextoNaTela(tag, texto) {
+    let campo = document.querySelector(tag);
+    campo.innerHTML = texto; //aqui eu defino que minha função irá receber dois valores, tag e texto. toda vez que eu for executar minha função, eu irei definir o que será a tag e o que será texto, como a seguir.
+    responsiveVoice.speak(texto, 'Brazilian Portuguese Female', {rate:1.0}) // essa linha indica que queremos que a função responsive voice, atribuída na linha 7 do html faça a leitura de tudo que for texto em nosso jogo. O rate, é a velocidade da leitura. 
+}
+
+function exibirMensagemInicial() {
+    exibirTextoNaTela('h1', 'Jogo do número secreto');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+}
+
+exibirMensagemInicial();
+
+function verificarChute() {
+    let chute = document.querySelector('input').value;
+    
+    if (chute == numeroSecreto) {
+        exibirTextoNaTela('h1', 'Acertou!');
+        let palavraTentativa = tentativas > 1 ? 'tentativas' : 'tentativa';
+        let mensagemTentativas = `Você descobriu o número secreto com ${tentativas} ${palavraTentativa}!`;
+        exibirTextoNaTela('p', mensagemTentativas);
+        document.getElementById('reiniciar').removeAttribute('disabled'); // aqui ativa o botão que estaria desabilitado no documento html
+    } else {
+        if (chute > numeroSecreto) {
+            exibirTextoNaTela('p', 'O número secreto é menor');
+        } else {
+            exibirTextoNaTela('p', 'O número secreto é maior');
+        }
+        tentativas++;
+        limparCampo();
+    }
+}
+
+function gerarNumeroAleatorio() {
+    let numeroEscolhido = parseInt(Math.random()* numeroLimite + 1);  // código para gerar um número aleatório entre 1 e 10;
+    let quantidadeDeElementosNaLista = listaNumerosSorteados.length;
+
+    if(quantidadeDeElementosNaLista == numeroLimite){
+        listaNumerosSorteados = [];
+    }
+
+    if (listaNumerosSorteados.includes(numeroEscolhido)){
+        return gerarNumeroAleatorio();
+    } else{
+        listaNumerosSorteados.push(numeroEscolhido);
+        console.log(listaNumerosSorteados);
+        return numeroEscolhido;
+    }
+}
+
+
+function limparCampo(){
+    chute = document.querySelector('input');
+    chute.value = '';
+}
+
+function reiniciarJogo(){
+    numeroSecreto = gerarNumeroAleatorio();
+    limparCampo();
+    tentativas = 1;
+    exibirMensagemInicial();
+    document.getElementById('reiniciar').setAttribute('disabled', true);
+}
